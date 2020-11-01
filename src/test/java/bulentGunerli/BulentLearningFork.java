7fd412e10a766262febc1542b4372deb3ff1cdc1
bugunerli <gunerlibulent@gmail.com>
2020-11-01 11:01:00 -0700

    Added some new codes in BulentLearningFork

--- src/test/java/bulentGunerli/BulentLearningFork.java
+++ src/test/java/bulentGunerli/BulentLearningFork.java
@@ -1,4 +1,10 @@
 package bulentGunerli;
 
 public class BulentLearningFork {
+
+    public static void main(String[] args) {
+        System.out.println("Bulent Gunerli is learning great information about Fork on gitHub.");
+    }
+
+
 }
